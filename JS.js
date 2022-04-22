a1930138fd76e9aebee34a83220f1cece9126de8
imshow123 <a0970098738@gmail.com>
2022-04-22 18:51:04 +0800

    首頁完成只差輪播

--- JS.js
+++ JS.js
@@ -6,8 +6,37 @@ function showMenu(){
             menu.classList.add('hidden');
         }
 }
-function init(){
-    document.getElementById('hb').onclick=showMenu;
+function showLarge(e){
+	document.getElementById('large').src = e.target.src;
 }
 
-window.addEventListener('load', init, false)
+
+
+
+
+function init(){
+    document.getElementById('hb').onclick=showMenu;
+
+    //點小圖秀大圖
+    let imgs = document.querySelectorAll('.product .item-pic img');
+	    for(i=0; i<imgs.length; i++){
+		    imgs[i].onmouseover = showLarge;
+	};
+
+
+}
+
+
+window.addEventListener('load', init, false);
+
+
+$(function (adVideo) {
+    let divWidth = $('.banner').width();
+    let imgCount = $('.video li').length();
+    $('.video').width(divWidth * imgCount);
+    $('.video li').width(divWidth);   
+    $('.video').animate({
+        left: divWidth * imgCount[i] * -1,
+    });
+});
+setInterval(adVideo,3000);
